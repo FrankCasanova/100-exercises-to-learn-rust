b91b40d34c2c10ed581db519f693ed066cc606b5
Frank Casanova <frankcasanova.info@gmail.com>
2025-02-20 14:18:32 +0100

    the first commit

--- exercises/04_traits/13_drop/src/lib.rs
+++ exercises/04_traits/13_drop/src/lib.rs
@@ -2,6 +2,28 @@
 //  unless a certain operation has been performed on it.
 //  You can see the expected API in the tests below.
 
+pub struct DropBomb {
+    defused: bool    
+}
+
+impl DropBomb {
+    pub fn new() -> Self {
+        DropBomb { defused: false }
+    }
+
+    pub fn defuse(&mut self) {
+        self.defused = true
+    }
+}
+
+impl Drop for DropBomb {
+    fn drop(&mut self) {
+        if !self.defused {
+            panic!("boom");
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -11,7 +33,7 @@ mod tests {
     fn test_drop_bomb() {
         let bomb = DropBomb::new();
         // The bomb should panic when dropped
-    }
+    } // <--- here is where the bomb is dropped
 
     #[test]
     fn test_defused_drop_bomb() {
